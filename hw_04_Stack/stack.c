bdbfe7d131e2919bec1dff7136aa9737a5bf0a6f
alexander-shepelev <64279078+alexander-shepelev@users.noreply.github.com>
2020-05-16 15:24:10 +0300

    Create stack.c

---
+++ hw_04_Stack/stack.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "stack.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+push(struct stack** head, const char str)
+{
+	struct stack* l = (struct stack*)malloc(sizeof(struct stack));
+	l->value = str;
+	l->next = *head;
+	*head = l;
+}
+
+char pop(struct stack** head)
+{
+	if (*head == NULL)
+		return 0;
+	else
+	{
+		struct stack* l = NULL;
+		struct stack* Q = NULL;
+		Q = *head;
+		char result = Q->value;
+		*head = Q->next;
+		l = *head;
+		return result;
+	}
+}
+
+empty(struct stack** head)
+{
+	if (*head != NULL)
+		return 0;
+	else
+		return 1;
+}
+
